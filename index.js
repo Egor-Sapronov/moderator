2559651aecd181a08c296f7fa32332a3d42e5896
Egor <sapronov.egor@gmail.com>
2015-05-04 19:06:49 +0600

    fix

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.use(bodyparser.urlencoded({
 
 app.get('/auth/google',
     passport.authenticate('google', {
-        scope: ['https://www.googleapis.com/auth/plus.login',
-            'https://www.googleapis.com/auth/plus.profile.emails.read']
+        scope: ['https://www.googleapis.com/auth/userinfo.profile',
+            'https://www.googleapis.com/auth/userinfo.email']
     }));
 
 app.get('/oauth2callback',
