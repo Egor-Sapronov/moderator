889a37df444678cefd9914122b9ebe0065dbc849
Egor <sapronov.egor@gmail.com>
2015-05-04 19:40:53 +0600

    fix

--- app.js
+++ app.js
@@ -4,11 +4,23 @@ let express = require('express');
 let app = express();
 let bodyparser = require('body-parser');
 let passport = require('./libs/auth/auth.es6').passport;
+let session = require('express-session');
+let cookieParser = require('cookie-parser');
+let methodOverride = require('method-override');
+let bodyParser = require('body-parser');
 
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({
-    extended: false
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride());
+app.use(session({
+    secret: 'secret',
+    cookie: true,
+    resave: false,
+    saveUninitialized: false
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/auth/google',
     passport.authenticate('google', {
