6793a7db5890cdb67322afb9a17468ee85a48c8b
Egor <sapronov.egor@gmail.com>
2015-05-16 16:20:02 +0600

    don"t forse db

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ let app = require('./app');
 let db = require('./libs/database.es6');
 
 db.sequelize
-    .sync({force: true})
     .then(function () {
         app.listen(process.env.PORT || 3000, function () {
             console.log('Express server listening on port ' + process.env.PORT);
